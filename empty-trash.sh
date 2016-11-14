2f2cf9d4c3d32beb9e51c1aa3ccf9c615e2deab7
Brian Boughton <brian@brianboughton.com>
2016-11-14 07:12:01 -0700

    Simplify and clarify
    Simplifying the code by moving complicated logic to modular functions.
    
    Clarifying the code and output by removing old comments and adding new
    logging statements.

--- empty-trash.sh
+++ empty-trash.sh
@@ -3,16 +3,35 @@
 date_iso8601() { /bin/date +%Y-%m-%dT%H:%M:%S%z; }
 log() { echo "$(date_iso8601) $@"; }
 
+find_trash() {
+	local query
+	query=$1
 
-# Find files in the local trash that haven't been modified in the last 30 days
-# Securely remove all the old files and directories with 7 passes
-#
-# Using -mindepth 1 -prune, so that if a DIRECTORY is in the Trash every file
-# in the directory is deleted (good for app bundles)
-# /usr/bin/find $HOME/.Trash -mindepth 1 -prune -not -newermt '30 days ago' -exec /usr/bin/srm --recursive --medium --zero --force {} \;
-log "looking for trash older then 15 days"
-/usr/bin/find $HOME/.Trash -mindepth 1 -prune -not -newermt "15 days ago" | while read file
-do
+	# Using -mindepth 1 -prune, so that if a DIRECTORY is in the Trash every 
+	# file in the directory is deleted (good for app bundles)
+	/usr/bin/find $HOME/.Trash -mindepth 1 -prune -not -newermt "${query}" 
+}
+
+secure_rm() {
+	local file
+	file=$1
+
 	log "deleting: $file"
 	/usr/bin/srm --recursive --medium --zero --force "$file"
+}
+
+# limiting number of items to remove so that we don't bog the system down
+limit_files=5
+days=15
+
+files=$(find_trash "${days} days ago" | head -n${limit_files})
+
+if [ -z "${files}" ]; then
+	log "no files found older then ${days} days"
+	exit
+fi
+
+echo "${files}" | while read file
+do
+	secure_rm "$file"
 done
